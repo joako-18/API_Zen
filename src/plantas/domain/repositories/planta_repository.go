02272c03e4457a896cd810b859a5c48bb3981112
joako-18 <joako1327@gmail.com>
2025-04-03 20:52:54 -0600

    bycript2

--- src/plantas/domain/repositories/planta_repository.go
+++ src/plantas/domain/repositories/planta_repository.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 )
 
-// Definir la interfaz
 type PlantaRepository interface {
 	GetAll() ([]entities.Planta, error)
 	Create(planta entities.Planta) error
@@ -13,17 +12,14 @@ type PlantaRepository interface {
 	Delete(id int) error
 }
 
-// Estructura que implementa la interfaz
 type PlantaRepositoryImpl struct {
 	db *sql.DB
 }
 
-// Constructor para la implementación de la interfaz
 func NewPlantaRepository(db *sql.DB) *PlantaRepositoryImpl {
 	return &PlantaRepositoryImpl{db: db}
 }
 
-// Implementación del método GetAll
 func (r *PlantaRepositoryImpl) GetAll() ([]entities.Planta, error) {
 	rows, err := r.db.Query("SELECT id, nombre, tipo, riego FROM plantas")
 	if err != nil {
@@ -42,19 +38,16 @@ func (r *PlantaRepositoryImpl) GetAll() ([]entities.Planta, error) {
 	return plantas, nil
 }
 
-// Implementación del método Create
 func (r *PlantaRepositoryImpl) Create(planta entities.Planta) error {
 	_, err := r.db.Exec("INSERT INTO plantas (nombre, tipo, riego) VALUES (?, ?, ?)", planta.Nombre, planta.Tipo, planta.Riego)
 	return err
 }
 
-// Implementación del método Update
 func (r *PlantaRepositoryImpl) Update(planta entities.Planta) error {
 	_, err := r.db.Exec("UPDATE plantas SET nombre=?, tipo=?, riego=? WHERE id=?", planta.Nombre, planta.Tipo, planta.Riego, planta.ID)
 	return err
 }
 
-// Implementación del método Delete
 func (r *PlantaRepositoryImpl) Delete(id int) error {
 	_, err := r.db.Exec("DELETE FROM plantas WHERE id=?", id)
 	return err
